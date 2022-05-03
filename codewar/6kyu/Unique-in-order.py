3969b8e63a86ed7f8791d5ea13b919f40ed2a77c
makaraduman <makaraduman@gmail.com>
2022-05-03 10:05:13 -0400

    Update Unique-in-order.py

--- codewar/6kyu/Unique-in-order.py
+++ codewar/6kyu/Unique-in-order.py
@@ -3,7 +3,7 @@ def unique_in_order(iterable):
     for i in iterable:
         if newList == []: # if the newList is empty
             newList.append(i) # Add the first item to the list
-        elif newList[-1] != i: # if the list item in the list is not equal to i:
+        elif newList[-1] != i: # if the last list item in the list is not equal to i:
             newList.append(i) # Add i to the list 
     return newList
 
